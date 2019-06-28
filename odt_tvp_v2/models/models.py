b08ffcdec4e5ade14002323ed409d8d20a1d9201
William <williamcolin954@gmail.com>
2019-06-28 08:19:25 -0500

    Se arregla la funcion para extraer los datos directos de cuentas analiticas, ademas se sustituyen los campos en la vista principal de control presupuestal por los nuvos campos

--- odt_tvp_v2/models/models.py
+++ odt_tvp_v2/models/models.py
@@ -1887,9 +1887,9 @@ class TablaGastos(models.Model):
 
 	#Test Control presupuestal
 
-	debit = fields.Float(string="Debe",compute="get_account_analityc")
-	credit = fields.Float(string="Haber",compute="get_account_analityc")
-	balance = fields.Float(string="Saldo pendiente",compute="get_account_analityc")
+	debit = fields.Float(string="Gasto Real",compute="get_account_analityc")
+	credit = fields.Float(string="Gasto Facturado",compute="get_account_analityc")
+	balance = fields.Float(string="Unidad Bruta Real",compute="get_account_analityc")
 
 	# Campos de presupuesto autorizado
 	btl = fields.Float(related='ref_project.btl', string='BTL/PDV')
